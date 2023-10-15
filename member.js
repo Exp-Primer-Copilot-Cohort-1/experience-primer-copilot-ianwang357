18d4fd86377052b5ece4b432b0755d882f177436
ianwang357 <91436737+ianwang357@users.noreply.github.com>
2023-10-15 05:43:37 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var member = document.getElementById("member");
+    var skillsMember = document.getElementById("skillsMember");
+
+    skills.style.display = "none";
+    member.style.display = "none";
+    skillsMember.style.display = "block";
+}
